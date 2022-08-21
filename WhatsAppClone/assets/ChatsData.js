76d9f4df51f8fbc5c80c0765a4b15a7f5d07a214
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-21 19:48:33 +0300

    Dummy data created.

---
+++ WhatsAppClone/assets/ChatsData.js
@@ -0,0 +1,155 @@
+const chatList = [
+  {
+    id: 'a1',
+    receiver: {
+      id: 'u1',
+      firstName: 'Jack',
+      lastName: 'Nicholson',
+      profileImgUrl: 'https://m.media-amazon.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_UY317_CR7,0,214,317_AL_.jpg',
+    },
+    messages: [
+      {
+        text: 'Hey dude!',
+        datetime: '2022-08-21T16:32:22.859Z',
+      },
+      {
+        text: "How's it going?",
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a2',
+    receiver: {
+      id: 'u2',
+      firstName: 'Charlize',
+      lastName: 'Theron',
+      profileImgUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5Mzc4ODU0Ml5BMl5BanBnXkFtZTcwNjU1NTI0Mw@@._V1_UY317_CR12,0,214,317_AL_.jpg',
+    },
+    messages: [
+      {
+        text: 'Hey!',
+        datetime: '2022-08-21T16:33:22.859Z',
+      },
+      {
+        text: 'I have good news. Call me ASAP!',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      {
+        text: 'Call me ASAP!',
+        datetime: '2022-08-21T16:35:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a3',
+    receiver: {
+      id: 'u3',
+      firstName: 'Edward',
+      lastName: 'Norton',
+      profileImgUrl: 'https://m.media-amazon.com/images/M/MV5BMTYwNjQ5MTI1NF5BMl5BanBnXkFtZTcwMzU5MTI2Mw@@._V1_UY317_CR16,0,214,317_AL_.jpg',
+    },
+    messages: [
+      {
+        text: 'Hello!',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      {
+        text: 'How are you?',
+        datetime: '2022-08-21T16:35:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a3',
+    receiver: {
+      id: 'u3',
+      firstName: 'Edward',
+      lastName: 'Norton',
+      profileImgUrl: '',
+    },
+    messages: [
+      {
+        text: 'Hello!',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      {
+        text: 'How are you?',
+        datetime: '2022-08-21T16:35:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a4',
+    receiver: {
+      id: 'u4',
+      firstName: 'Anthony',
+      lastName: 'Hopkins',
+      profileImgUrl: 'https://m.media-amazon.com/images/M/MV5BMTg5ODk1NTc5Ml5BMl5BanBnXkFtZTYwMjAwOTI4._V1_UY317_CR6,0,214,317_AL_.jpg',
+    },
+    messages: [
+      {
+        text: 'Hello!',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      {
+        text: 'Dou you have notes?',
+        datetime: '2022-08-21T16:35:22.859Z',
+      },
+      {
+        text: ':) :) :)',
+        datetime: '2022-08-21T16:36:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a5',
+    receiver: {
+      id: 'u5',
+      firstName: 'James',
+      lastName: 'Franco',
+      profileImgUrl: '',
+    },
+    messages: [
+      {
+        text: 'How is the weather like in Istanbul?',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+    ],
+  },
+  {
+    id: 'a6',
+    receiver: {
+      id: 'u6',
+      firstName: 'Naomi',
+      lastName: 'Watts',
+      profileImgUrl: 'https://www.imdb.com/name/nm0915208/mediaviewer/rm2785845760?ref_=nm_ov_ph',
+    },
+    messages: [
+      {
+        text: "What's up?",
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      ,
+    ],
+  },
+  {
+    id: 'a7',
+    receiver: {
+      id: 'u7',
+      firstName: 'Quentin',
+      lastName: 'Tarantino',
+      profileImgUrl: 'https://m.media-amazon.com/images/M/MV5BMTgyMjI3ODA3Nl5BMl5BanBnXkFtZTcwNzY2MDYxOQ@@._V1_UX214_CR0,0,214,317_AL_.jpg',
+    },
+    messages: [
+      {
+        text: 'Okay',
+        datetime: '2022-08-21T16:34:22.859Z',
+      },
+      {
+        text: 'Thanks.',
+        datetime: '2022-08-21T16:35:22.859Z',
+      },
+    ],
+  },
+];
