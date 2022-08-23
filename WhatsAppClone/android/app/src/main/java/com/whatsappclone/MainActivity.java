0a89222f3ed6de869535f3613e90ab4ca01abc7d
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-23 20:07:21 +0300

    Stack Navigation implemented.

--- WhatsAppClone/android/app/src/main/java/com/whatsappclone/MainActivity.java
+++ WhatsAppClone/android/app/src/main/java/com/whatsappclone/MainActivity.java
@@ -3,7 +3,7 @@ package com.whatsappclone;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -13,6 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "WhatsAppClone";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
