b2578e34bf05422db3e0b642a91c0aa46c89b90b
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-23 22:12:18 +0300

    HomePage ChatList first design.

--- WhatsAppClone/components/Home/HomePage.js
+++ WhatsAppClone/components/Home/HomePage.js
@@ -1,12 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Header from './Header';
+import ChatList from './ChatList';
 const HomePage = () => {
   return (
     <SafeAreaView>
       <Header />
+      <ScrollView>
+        <ChatList/>
+      </ScrollView>
       <Text>HomePage</Text>
       <Icon name="instagram" size={28} color="#f9524d" />
     </SafeAreaView>
