403b8be8cdcf62554bd9450725e53f2dab642970
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-24 19:43:08 +0300

    ChatListItem avatar added.

--- WhatsAppClone/components/Home/ChatList/ChatListItem.js
+++ WhatsAppClone/components/Home/ChatList/ChatListItem.js
@@ -1,12 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const ChatListItem = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.imageWrapper}>
-        <Text>Image</Text>
-      </View>
+      <View style={styles.imageContainer}>
+      <Image
+      style={styles.image}
+      source={{
+        uri:""
+      }}
+      />
+    </View>
       <View style={styles.titleAndTextWrapper}>
         <Text style={styles.titleText}>UserName</Text>
         <Text style={styles.msgText}>Last Text</Text>
@@ -19,12 +24,12 @@ export default ChatListItem;
 
 const styles = StyleSheet.create({
   container: {
-    height: 250,
     borderBottomWidth: 1,
     borderBottomColor: 'black',
     flexDirection: 'row',
     paddingTop: 16,
     paddingLeft: 16,
+    paddingBottom:16
   },
   imageWrapper: {},
   titleAndTextWrapper: {
@@ -34,4 +39,17 @@ const styles = StyleSheet.create({
     fontWeight: '800',
   },
   msgText: {},
+  imageContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: 'black',
+    
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
 });
